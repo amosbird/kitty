74556384916b5d7b0b201efc15491cf81aa15819
Amos Bird <amosbird@gmail.com>
2022-11-25 15:49:42 +0800

    Update IME position

--- kitty/keys.c
+++ kitty/keys.c
@@ -215,6 +215,11 @@ on_key_input(GLFWkeyevent *ev) {
             debug("invalid state, ignoring\n");
             return;
     }
+
+    if (ev->key == 0) {
+        return;
+    }
+
     bool dispatch_ok = true, consumed = false;
 #define dispatch_key_event(name) { \
     PyObject *ke = NULL, *ret = NULL; \
