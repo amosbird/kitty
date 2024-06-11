67b8ed8b855256cf3bf96d581b6c289e85bd9c15
Amos Bird <amosbird@gmail.com>
2024-06-11 17:36:00 +0800

    Turn select-window into show-window-title

--- kitty/boss.py
+++ kitty/boss.py
@@ -1626,14 +1626,14 @@ class Boss:
         if initial_os_window_id != tab.os_window_id:
             self.focus_os_window(tab.os_window_id, False)
         self.current_visual_select = VisualSelect(tab.id, tab.os_window_id, initial_tab_id, initial_os_window_id, choose_msg, callback, reactivate_prev_tab)
-        if tab.current_layout.only_active_window_visible:
-            self.select_window_in_tab_using_overlay(tab, choose_msg, only_window_ids)
-            return
         km = KeyboardMode('__visual_select__')
         km.on_action = 'end'
         km.keymap[SingleKey(key=GLFW_FKEY_ESCAPE)].append(KeyDefinition(definition='visual_window_select_action_trigger 0'))
         fmap = get_name_to_functional_number_map()
         alphanumerics = get_options().visual_window_select_characters
+        wid = tab.active_window.id if tab.active_window else 1
+        km.keymap[SingleKey(mods=GLFW_MOD_CONTROL, key=ord('c'))].append(KeyDefinition(definition=f'visual_window_select_action_trigger {wid}'))
+        km.keymap[SingleKey(key=57344)].append(KeyDefinition(definition=f'visual_window_select_action_trigger {wid}'))
         for idx, window in tab.windows.iter_windows_with_number(only_visible=True):
             if only_window_ids and window.id not in only_window_ids:
                 continue
@@ -1641,19 +1641,13 @@ class Boss:
             if idx >= len(alphanumerics):
                 break
             ch = alphanumerics[idx]
-            window.screen.set_window_char(ch)
+            window.screen.set_window_char(' ')
             self.current_visual_select.window_ids.append(window.id)
             for mods in (0, GLFW_MOD_CONTROL, GLFW_MOD_CONTROL | GLFW_MOD_SHIFT, GLFW_MOD_SUPER, GLFW_MOD_ALT, GLFW_MOD_SHIFT):
                 km.keymap[SingleKey(mods=mods, key=ord(ch.lower()))].append(ac)
                 if ch in string.digits:
                     km.keymap[SingleKey(mods=mods, key=fmap[f'KP_{ch}'])].append(ac)
-        if len(self.current_visual_select.window_ids) > 1:
-            self.mappings._push_keyboard_mode(km)
-            redirect_mouse_handling(True)
-            self.mouse_handler = self.visual_window_select_mouse_handler
-        else:
-            self.visual_window_select_action_trigger(self.current_visual_select.window_ids[0] if self.current_visual_select.window_ids else 0)
-            self.ring_bell_if_allowed(tab.os_window_id)
+        self.mappings._push_keyboard_mode(km)
 
     def ring_bell_if_allowed(self, os_window_id: int = 0) -> bool:
         if get_options().enable_audio_bell:
